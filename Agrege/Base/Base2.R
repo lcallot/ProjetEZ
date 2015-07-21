ff3a787c8c626be45e65284e33ab33e3c28452e4
clementcarrier <clement.carrier@ensae.fr>
2015-07-21 11:27:48 +0200

    maj

--- Agrege/Base/Base2.R
+++ Agrege/Base/Base2.R
@@ -1,10 +1,9 @@
 #### AWM DATABASE ####
 
-data <- read.csv("~/Documents/Stage VU/Todo/AWM/AWM18UP14.csv")
+data <- read.csv("Agrege/Base/AWM/AWM18UP14.csv")
 
 #creation of the variable "compensation per employee" & "real consumption" & "real investment"
 data$CPE=data$WIN/data$LEN
-
 
 
 # extracting subset of AWM data
@@ -38,7 +37,7 @@ XTR<-log(subsets[,"XTR"])
 #### ECB DATABASE ####
 
 #Monetary Agregate
-dataMagreg <- read.csv("~/Documents/Stage VU/Todo/ECB/dataMagreg.csv", sep=";")
+dataMagreg <- read.csv("Agrege/Base/ECB/dataMagreg.csv", sep=";")
 dataMagreg$X<-NULL
 
 monthly <- ts(dataMagreg,start=c(1970,1),frequency=12)
@@ -50,7 +49,7 @@ M3<-logquarterly[,"M3"]
 
 
 #Loans
-loans <- read.csv("~/Documents/Stage VU/Todo/ECB/loans.csv", sep=";")
+loans <- read.csv("Agrege/Base/ECB/loans.csv", sep=";")
 loans$date<-NULL
 
 monthly2 <- ts(loans[2:196,],start=c(1997,10),frequency=12)
@@ -61,7 +60,7 @@ LHO<-logquarterly2[,"LHO"]
 
 
 #Price Producer Index
-PPI <- read.csv("~/Documents/Stage VU/Todo/ECB/PPI.csv")
+PPI <- read.csv("Agrege/Base/ECB/PPI.csv")
 
 monthly3 <- ts(rev(PPI[,2]),start=c(1981,1),frequency=12)
 quarterly3 <- aggregate(monthly3, nfrequency=4)/3
@@ -71,7 +70,7 @@ PPI <- log(quarterly3)
 
 
 #### IMF ####
-LIB3M <- read.csv("~/Documents/Stage VU/Todo/IMF/LIB3M.csv")
+LIB3M <- read.csv("Agrege/Base/IMF/LIB3M.csv")
 LIB<-ts(LIB3M,start=c(1970,1),frequency=4)/100
 
 
@@ -79,7 +78,7 @@ LIB<-ts(LIB3M,start=c(1970,1),frequency=4)/100
 #### EUROSTAT ####
 
 # Economic sentiment indicator
-ESIdata <- read.csv("~/Documents/Stage VU/Todo/EUROSTAT/ESIdata.csv")
+ESIdata <- read.csv("Agrege/Base/EUROSTAT/ESIdata.csv")
 indiceco<-ESIdata$Value
 ESIts<-ts(rev(indiceco),start=c(1985,1),frequency=12)
 
@@ -89,19 +88,18 @@ ESI <- ESIq/100
 
 
 #### DATASTREAM ####
-dowEuroStoxx <- read.csv("~/Documents/Stage VU/Todo/Datastream/dowEuroStoxx.csv", sep=";", dec=",")
+dowEuroStoxx <- read.csv("Agrege/Base/Datastream/dowEuroStoxx.csv", sep=";", dec=",")
 DJES<-ts(dowEuroStoxx[1:108,2],start=c(1987,1),frequency=4)
 DJES<-log(DJES)
 
 
 #### FUSION BASE ####
 
+# Q1 1970 to Q1 2015
 vardata<-as.ts(cbind(COMPR, PCOMU, POILU, HEG, HEXSA, HICPSA, HICP,EEN, EXR, STN, LTN , URX, YER, XTR, MTR, CPE, YWRX, LHO, LFI , M1 , M3 ,ESI, LIB, PPI, DJES ))
 vardataframe<-data.frame(vardata[1:176,])
 
 
-
-setwd("~/Documents/Stage VU/Todo/Result")
-save(vardataframe,file="vardata3.Rdata")
+save(vardataframe,file="Agrege/Base/vardata.Rdata")
 
 
