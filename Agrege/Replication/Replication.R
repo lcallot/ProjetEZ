28b0347ff39247a68c29b3443c1ad9a5da199869
clementcarrier <clement.carrier@ensae.fr>
2015-07-24 16:13:25 +0200

    maj

--- Agrege/Replication/Replication.R
+++ Agrege/Replication/Replication.R
@@ -7,10 +7,26 @@ library('urca')
 
 load("Agrege/Base/vardata.Rdata")
 
-sub<- var[,c('date','M1','M3','STN','LTN','YED','YER')]
+sub1<- var[,c('date','M1','M3','STN','LTN','YED','YER')]
 
-# data from Q1 1990 to 2013 Q4
-sub<-sub[81:176,]
+# data from Q1 1990 to 2013 Q4 (81:176)
+sub<-sub1[81:176,]
+
+# dif data from Q1 1990 to 2013 Q4
+sub2<-sub1[80:176,]
+dsub <- tail(sub2[,-1],-1) - head(sub2[,-1],-1)
+date<-sub2[-1,1]
+dsub<-data.frame(date,dsub)
+
+# dif dif data from Q1 1990 to 2013 Q4
+sub3<-sub1[79:176,]
+dsub2 <- tail(sub3[,-1],-1) - head(sub3[,-1],-1)
+ddsub <- tail(dsub2,-1) - head(dsub2,-1)
+ddsub<-data.frame(date,ddsub)
+
+rownames(sub)<-NULL
+rownames(dsub)<-NULL
+rownames(ddsub)<-NULL
 
 # type = P-test  for ERS  &  DF-GLS 
 erstest<-function(data,trend,type,lagmax){
@@ -26,78 +42,50 @@ erstest<-function(data,trend,type,lagmax){
 
 
 erstest(sub[,-1],"trend","P-test",10)
-erstest(sub[,-1],"trend","DF-GLS",10)
-
-
-# dif data from Q1 1990 to 2013 Q4
-sub2<- var[,c('date','M1','M3','STN','LTN','YED','YER')]
-sub2<-sub2[80:176,]
-dsub <- tail(sub2[,-1],-1) - head(sub2[,-1],-1)
-date<-sub2[-1,1]
-dsub<-data.frame(date,dsub)
-rownames(dsub)<-NULL
-
 erstest(dsub[,-1],"constant","P-test",10)
-erstest(dsub[,-1],"constant","DF-GLS",10)
+erstest(ddsub[,-1],"constant","P-test",10)
 
 
 
 #### Replication 
 
-RealMon <- sub$M3-sub$YED
+## Johansen test
+RM3 <- sub$M3-sub$YED
 Spread <- sub$LTN - sub$STN
 pi<-4*dsub$YED
 GDP <- sub$YER
+Z=data.frame(RM3,Spread,pi,GDP)
+Z<-as.ts(Z)
+summary(ca.jo(Z,type="trace",ecdet="trend",K=2))
 
-reg<-lm(RealMon ~ GDP + Spread + pi)
+help(ca.jo)
+
+
+# Long term relation estimation
+
+
+reg<-lm(RM3 ~ GDP + Spread + pi + 0)
 summary(reg)
-
-library('tseries')
-## Phillips-Ouliaris
-z=c(RealMon,Spread,pi,GDP)
-Z=matrix(z,nrow=96 , ncol=4)
-po.test(Z, demean = TRUE, lshort = FALSE) 
-
-##### Test de Johansen
-Z1<-as.ts(Z)
-summary(ca.jo(Z1,type="trace",ecdet="trend",K=2))
+res<-reg$residuals
 
 
 
 
+# ECM
+dRM3 <- dsub$M3-dsub$YED
+dRS<-dsub$STN
+dRL<-dsub$LTN
+dpi<-4*ddsub$YED
+dGDP <- dsub$YER
+ddGDP <- ddsub$YER
 
 
-#regression de lrev sur lconso
-reg<-lm(C~R)
-summary(reg)
+a<-(dRS[-(1:2)] + lag(dRS)[-(1:2)])/2
+b<-(dpi[-(1:2)]+lag(dpi)[-(1:2)])/2
 
-## recuperation des residus estimes
-residu<-reg$residuals
+ecm<-lm(dRM3[-(1:2)] ~ ddGDP[-(1:2)] + a + b +dRL[-(1:2)] + lag(res,lag=2)[-(1:2)])
 
-
-#ADF
-#Modele 3:
-a=ur.df(residu, type = "trend", lags = 10, selectlags ="BIC");summary(a)
-
-#Modele 2:
-a=ur.df(residu, type = "drift", lags = 10, selectlags ="BIC");summary(a)
-
-#Modele 1:
-a=ur.df(residu, type = "none", lags = 10, selectlags ="BIC");summary(a)
-
-library(fUnitRoots)
-unitrootTest(residu, lags=1,type="ct")
-unitrootTest(residu, lags=1,type="c")
-unitrootTest(residu, lags=1,type="nc")
-
-unitrootTable(trend="ct",statistic="t")
-unitrootTable(trend="c",statistic="t")
-unitrootTable(trend="nc",statistic="t")
-
-
-
-
-
+summary(ecm)
 
 
 
