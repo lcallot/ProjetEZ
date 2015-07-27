e98f409c4e2b4a7016ed42bef18364697e7449e2
clementcarrier <clement.carrier@ensae.fr>
2015-07-27 16:24:44 +0200

    maj

--- Agrege/Replication/Bootstrap.R
+++ Agrege/Replication/Bootstrap.R
@@ -5,7 +5,33 @@ sub<-var[81:176,]
 sub<- sub[,c('date','M1','M3','STN','LTN','YED','YER','URX','POILU','LHO', 'LFI')]
 
 
-lm<-lm(sub$YED ~ sub$M3 + sub$YED)
+lm<-lm(sub$YED ~ sub$M3 + sub$YER)
+pred<-as.matrix(lm$fitted.value)
+coef<-lm$coef
+e<-lm$res
+etild<-e-mean(e)
+
+iter<-10000
+t=matrix(NA,3,iter)
+estar=matrix(NA,length(etild),iter)
+ystar=matrix(NA,length(etild),iter)
+
+for (i in 1:iter){
+  
+  estar[,i]<-as.matrix(sample(etild,length(etild),replace = T))
+  ystar[,i]<-pred+estar[,i]
+  
+  boot<-lm(ystar[,i]~sub$M3 + sub$YER)
+  bootcoef<-boot$coef
+  t[,i]<-as.matrix(sqrt(length(ystar[,1]))*(bootcoef-coef))
+  r[,i]<-
+}
+
+plot(density(t[1,]))
+
+
+
+
 
 
 
