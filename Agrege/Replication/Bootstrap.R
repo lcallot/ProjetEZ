3745ed1d9daf47e01b0454afa54c552a7be99651
clementcarrier <clement.carrier@ensae.fr>
2015-07-28 14:43:07 +0200

    maj

--- Agrege/Replication/Bootstrap.R
+++ Agrege/Replication/Bootstrap.R
@@ -1,38 +1,58 @@
 library('glmnet')
 library('MASS')
 
-rw<-function(t,x,mean,var){
-  y<-matrix(0,1,t)
-  y[,1]<-x
-    for(i in 2:t){
-      y[,i] <- y[,i-1] + mvrnorm(n = 1, mu= mean , Sigma= var)
-    }
-  return(t(y))
-}
+source("laurent/lasso.R")
+
 
 # Simulated data
+norm<-NULL
+p=50
+n=100
+for (i in 1:p){
+  norm<-cbind(norm,as.matrix(rnorm(n,0,1),n,p))
+}
+norm<-data.frame(norm)
 
-a<-rnorm(100,0,1)
-b<-rnorm(100,0,1)
-c<-rw(100,0,0,1)
-d<-rw(100,0,0,1)
 
-fun<-function(y,x,iter,lasso){
-  estar=matrix(NA,length(y),iter)
-  ystar=matrix(NA,length(y),iter)
-  u=matrix(NA,2,iter)
+rw=NULL
+for (i in 1:p){
+  rw<-cbind(rw,RW(n,0,0,1))
+}
+rw<-data.frame(rw)
+
+
+fun<-function(df,iter,lasso){
+  estar=matrix(NA,length(df[,1]),iter)
+  ystar=matrix(NA,length(df[,1]),iter)
+  u=matrix(NA,dim(df)[2],iter)
+  a=NULL
   if (lasso==TRUE){
-    
-  } else {
-    OLS<-lm(a~b)
+    a<-df[,1]
+    LASSO<-lasso(a ~ . , df)
+    prediction<-as.matrix(LASSO$y-LASSO$residuals)
+    elasso<-LASSO$res
+    residu<-elasso-mean(elasso)
+    for (i in 1:iter){
+      estar[,i]<-as.matrix(sample(residu,length(residu),replace = T))
+      ystar[,i]<-prediction+estar[,i]
+      z<-ystar[,i]
+      df2<-data.frame(z,df[,-1])
+      boot<-lasso(z ~ . , df2 )
+      bootcoef<-boot$coef
+      u[,i]<-as.matrix(bootcoef)
+    }
+    } else {
+    a<-df[,1]
+    OLS<-lm(a~. , df)
     prediction<-as.matrix(OLS$fitted.value)
-    coef<-OLS$coef
     eols<-OLS$res
     residu<-eols-mean(eols)
     for (i in 1:iter){
       estar[,i]<-as.matrix(sample(residu,length(residu),replace = T))
       ystar[,i]<-prediction+estar[,i]
-      boot<-lm(ystar[,i]~x)
+      z<-ystar[,i]
+      df2<-data.frame(z,df[,-1])
+      boot<-lm(z ~ . , df2 )
       bootcoef<-boot$coef
       u[,i]<-as.matrix(bootcoef)
     }
@@ -40,19 +60,19 @@ fun<-function(y,x,iter,lasso){
   return(u)
 }
 
-result<-fun(a,b,10000,FALSE)
+ols<-fun(norm,100,FALSE)
+plot(density(ols[1,]))
+plot(density(ols[2,]))
+plot(density(ols[3,]))
 
-plot(density(result[1,]))
-plot(density(result[2,]))
+lasso<-fun(norm,100,TRUE)
+plot(density(lasso[1,]))
+plot(density(lasso[2,]))
+plot(density(lasso[3,]))
 
 
-resultrw<-fun(c,d,1000,FALSE)
+resultrw<-fun(c,d,10000,FALSE)
 plot(density(resultrw[1,]))
 plot(density(resultrw[2,]))
-
-
-
-
-
 
 
