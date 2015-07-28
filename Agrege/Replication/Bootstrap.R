6aacf746753a15754bb59983b4cc0b39bea41a60
clementcarrier <clement.carrier@ensae.fr>
2015-07-28 16:40:50 +0200

    maj

--- Agrege/Replication/Bootstrap.R
+++ Agrege/Replication/Bootstrap.R
@@ -7,7 +7,7 @@ source("Agrege/Replication/fun.R")
 
 # Simulated data
 norm<-NULL
-p=5
+p=50
 n=100
 for (i in 1:p){
   norm<-cbind(norm,as.matrix(rnorm(n,0,1),n,p))
@@ -25,16 +25,21 @@ rw<-data.frame(rw)
 
 
 # 
-ols<-fun(norm,5000,FALSE)
-
-lasso<-fun(norm,5000,TRUE)
+ols<-fun(rw,5000,FALSE)
+las<-fun(rw,5000,TRUE)
 
 # True distribution
-X<-as.matrix(cbind(matrix(rep(1,n)),norm[,-1]))
-true<-mvrnorm(n = 5000, 0*rep(1,p), Sigma = solve(t(X)%*%X))
+X<-as.matrix(cbind(matrix(rep(1,n)),rw[,-1]))
+true<-mvrnorm(n = 1000000, 0*rep(1,p), Sigma = solve(t(X)%*%X) )
 
 
 plot(density(ols[2,]))
-plot(density(lasso[2,]))
+plot(density(las[2,]))
 plot(density(true[2,]))
+
+
+
+
+
+
 
