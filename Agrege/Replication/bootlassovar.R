14f4ca2ae36b149c5b4ebcbe469ad21b63334580
clementcarrier <clement.carrier@ensae.fr>
2015-07-29 17:02:14 +0200

    maj

--- Agrege/Replication/bootlassovar.R
+++ Agrege/Replication/bootlassovar.R
@@ -1,24 +1,38 @@
-df<-norm
-bootlassovar<-function(df,iter,lasso){
-  estar=matrix(NA,length(df[,1]),iter)
-  ystar=matrix(NA,length(df[,1]),iter)
-  u=matrix(NA,dim(df)[2],iter)
-
-    LASSO<-lasso( X1 ~ . , df)
-    prediction<-as.matrix(LASSO$y-LASSO$residuals)
-    elasso<-LASSO$res
-    residu<-elasso-mean(elasso)
-    for (i in 1:iter){
-      estar[,i]<-as.matrix(sample(residu,length(residu),replace = T))
-      ystar[,i]<-prediction+estar[,i]
-      z<-ystar[,i]
-      df2<-data.frame(z,df[,-1])
-      boot<-lasso(z ~ . , df2 )
-      bootcoef<-boot$coef
-      u[,i]<-as.matrix(bootcoef)
+bootlassovar<-function(data,lag,iter,adap){
+  
+  lv<-lassovar(data,lags=lag,adaptive=adap)
+  res<-residuals(lv)
+  pred<-lv$y-res
+  coef<-as.matrix(lv$coefficients[2:(lag*dim(data)[2]+1),],lag*dim(data)[2],dim(data)[2])
+  intercept<-matrix(lv$coefficients[1,],1, dim(data)[2])
+  
+  residu<-NULL
+  for (l in 1:dim(res)[2]){
+    residu<-cbind(residu,res[,l]-mean(res[,l]))
+  }
+  
+  for (i in 1:iter){
+    estar=matrix(0,dim(data)[1],dim(data)[2])
+    ystar=matrix(0,dim(data)[1],dim(data)[2])
+    for (j in 1:dim(data)[1]){
+      estar[j,]<-residu[sample(seq(from=1,to=dim(residu)[1],by=1),1,replace = T),]
     }
-
-  return(u)
-}
-
-
+    
+    for (m in 1:lag){
+      ystar[m,]<-as.matrix(data[m,])+estar[m,]
+    }
+    
+    for (t in (lag+1):dim(data)[1]){
+      
+      for (l in 1:lag){
+        ystar[t,]<-ystar[t,] + ystar[t-l,]%*%coef[(1+(l-1)*dim(data)[2]):(dim(data)[2]*l),]
+      } 
+      ystar[t,]<-intercept + ystar[t,]
+      ystar[t,]<- ystar[t,] + estar[t,]
+    }
+    
+    lvboot<-lassovar(ystar,lags=lag,adaptive=adap)
+    print(lvboot$coef)
+  }
+  
+}  
--- Agrege/Replication/prediction.R
+++ Agrege/Replication/prediction.R
@@ -8,7 +8,7 @@ library('urca')
 library('stats')
 
 source("Agrege/Replication/forecastfunction.R")
-
+source("Agrege/Replication/bootlassovar.R")
 
 sub1<- var[,c('date','M3','STN','LTN','YED')]
 
@@ -25,67 +25,5 @@ mvar1 <- melt(var1,  id = 'time', variable.name = 'series')
 ggplot(mvar1, aes(time,value)) + geom_line() + facet_grid(series ~ . ,scales="free")
 
 
-
-df<-sub[,-1]
-iter=50
-
-lag=2
-
-bootlassovar<-function(df,iter,lasso){
-  estar=matrix(NA,dim(df)[1]-lag,dim(df)[2])
-  ystar=matrix(0,dim(df)[1],dim(df)[2])
-  ystar[(1:lag),]<-as.matrix(df[(1:lag),])
-  u=matrix(NA,dim(df)[2],iter)
-  
-  lv<-lassovar(sub[,-1],lags=2)
-  res<-residuals(lv)
-  pred<-lv$y-res
-  coef<-as.matrix(lv$coefficients[2:(lag*dim(df)[2]+1),],lag*dim(df)[2],dim(df)[2])
-  intercept<-matrix(lv$coefficients[1,],1, dim(df)[2])
-  residu<-NULL
-  for (l in 1:dim(res)[2]){
-    residu<-cbind(residu,res[,l]-mean(res[,l]))
-  }
-  
-
-  for (i in 1:iter){
-    
-    
-    for (j in 1:dim(residu)[1]){
-      estar[j,]<-residu[sample(seq(from=1,to=dim(residu)[1],by=1),1,replace = T),]
-    }
-    
-    for (t in (lag+1):dim(df)[1]){
-      
-      for (l in 1:lag){
-        ystar[t,]<-ystar[t,] + ystar[t-l,]%*%coef[(1+(l-1)*dim(df)[2]):(dim(df)[2]*l),]
-      } 
-      ystar[t,]<-intercept + ystar[t,]
-      ystar[t,]<- ystar[t,] + estar[t-lag,]
-    }
-    
-    lassovar(ystar,lags=lag)
-  }
-  
-  
-  
-    
-    help(sample)
-    
-    ystar[,i]<-prediction+estar[,i]
-    z<-ystar[,i]
-    df2<-data.frame(z,df[,-1])
-    boot<-lasso(z ~ . , df2 )
-    bootcoef<-boot$coef
-    u[,i]<-as.matrix(bootcoef)
-  }
-  
-  return(u)
-}
-
-
-
-help(lassovar)
-
-
-
+# bootlassovar
+bootlassovar(sub[,-1],4,10,"lasso")
