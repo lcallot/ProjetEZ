5173af80d09417ce524760644be269b88ef9613c
clementcarrier <clement.carrier@ensae.fr>
2015-07-29 17:44:08 +0200

    maj

--- Agrege/Replication/Bootstrap.R
+++ Agrege/Replication/Bootstrap.R
@@ -12,29 +12,30 @@ n=100
 for (i in 1:p){
   norm<-cbind(norm,as.matrix(rnorm(n,0,1),n,p))
 }
-norm<-data.frame(norm)
+beta<-matrix(c(1,-1,rep(0,p-2)))
 
+y<-norm%*%beta+
 
-rw=NULL
-for (i in 1:p){
-  rw<-cbind(rw,RW(n,0,0,1))
-}
-rw<-data.frame(rw)
+#rw=NULL
+#for (i in 1:p){
+ # rw<-cbind(rw,RW(n,0,0,1))
+#}
+#rw<-data.frame(rw)
 
-
+df<-data.frame(y,norm)
 
 
 # 
-ols<-fun(rw,1000,FALSE)
-las<-fun(rw,1000,TRUE)
+ols<-fun(df,500,FALSE)
+las<-fun(df,500,TRUE)
 
 # True distribution
 X<-as.matrix(cbind(matrix(rep(1,n)),rw[,-1]))
 true<-mvrnorm(n = 1000000, 0*rep(1,p), Sigma = diag(1,p) )
 solve(t(X)%*%X)
 
-plot(density(ols[2,]))
-plot(density(las[2,]))
+plot(density(ols[3,]))
+plot(density(las[3,]))
 plot(density(true[2,]))
 
 ciols=NULL
--- Agrege/Replication/fun.R
+++ Agrege/Replication/fun.R
@@ -4,32 +4,28 @@ fun<-function(df,iter,las){
   ystar=matrix(NA,length(df[,1]),iter)
   u=matrix(NA,dim(df)[2],iter)
   if (las==TRUE){
-    LASSO<-lasso( X1 ~ . , df)
+    LASSO<-lasso( y ~ . , df)
     prediction<-as.matrix(LASSO$y-LASSO$residuals)
-    elasso<-LASSO$res
-    residu<-elasso-mean(elasso)
+    residu<-LASSO$res-mean(LASSO$res)
     for (i in 1:iter){
       estar[,i]<-as.matrix(sample(residu,length(residu),replace = T))
       ystar[,i]<-prediction+estar[,i]
       z<-ystar[,i]
       df2<-data.frame(z,df[,-1])
       boot<-lasso(z ~ . , df2 )
-      bootcoef<-boot$coef
-      u[,i]<-as.matrix(bootcoef)
+      u[,i]<-as.matrix(boot$coef)
     }
   } else {
-    OLS<-lm( X1 ~ . , df)
+    OLS<-lm( y ~ . , df)
     prediction<-as.matrix(OLS$fitted.value)
-    eols<-OLS$res
-    residu<-eols-mean(eols)
+    residu<-OLS$res-mean(OLS$res)
     for (i in 1:iter){
       estar[,i]<-as.matrix(sample(residu,length(residu),replace = T))
       ystar[,i]<-prediction+estar[,i]
       z<-ystar[,i]
       df2<-data.frame(z,df[,-1])
       boot<-lm(z ~ . , df2 )
-      bootcoef<-boot$coef
-      u[,i]<-as.matrix(bootcoef)
+      u[,i]<-as.matrix(boot$coef)
     }
   }
   return(u)
