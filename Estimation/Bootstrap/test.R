6fd2f22b8c60b031ef19b6448cee0c3687d4daba
clementcarrier <clement.carrier@ensae.fr>
2015-08-05 21:24:01 +0200

    maj

--- Estimation/Bootstrap/test.R
+++ Estimation/Bootstrap/test.R
@@ -16,19 +16,32 @@ n=100
 beta=1
 p=50
 nonzero=5
-mc1<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
+mc1<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
 save(mc1,file = "Estimation/Bootstrap/mc.R")
 
 p=100
-mc2<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
+mc2<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
 save(mc2,file = "Estimation/Bootstrap/mc.R")
 
 
 p=200
-mc3<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
+mc3<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
 save(mc3,file = "Estimation/Bootstrap/mc.R")
 
 
+p=50
+nonzero=10
+mc4<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid10")
+save(mc4,file = "Estimation/Bootstrap/mc.R")
+
+p=100
+mc5<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid10")
+save(mc5,file = "Estimation/Bootstrap/mc.R")
+
+
+p=200
+mc6<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid10")
+save(mc6,file = "Estimation/Bootstrap/mc.R")
 
 
 
