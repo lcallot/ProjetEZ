262eba7af490c0e1c3f5681ce3c4ef43ce35895a
clementcarrier <clement.carrier@ensae.fr>
2015-08-05 21:21:33 +0200

    maj

--- Estimation/Bootstrap/test.R
+++ Estimation/Bootstrap/test.R
@@ -10,13 +10,26 @@ source("Functions/lahiri.R")
 source("Functions/montecarlo.R")
 
 
-iter=10
-boot=10
+iter=1000
+boot=100
 n=100
 beta=1
 p=50
 nonzero=5
-mc<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
-save(mc,file = "Estimation/Bootstrap/mc.R")
+mc1<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
+save(mc1,file = "Estimation/Bootstrap/mc.R")
+
+p=100
+mc2<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
+save(mc2,file = "Estimation/Bootstrap/mc.R")
+
+
+p=200
+mc3<-MC(iter,p,5,beta,n,boot,0.1,"iid5")
+save(mc3,file = "Estimation/Bootstrap/mc.R")
+
+
+
+
 
 
