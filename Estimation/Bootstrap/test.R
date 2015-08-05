a1757667bb756e228eb0812473bd77561f6a9e78
clementcarrier <clement.carrier@ensae.fr>
2015-08-05 22:07:27 +0200

    maj

--- Estimation/Bootstrap/test.R
+++ Estimation/Bootstrap/test.R
@@ -17,31 +17,31 @@ beta=1
 p=50
 nonzero=5
 mc1<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
-save(mc1,file = "Estimation/Bootstrap/mc1.R")
+save(mc1,file = "Estimation/Bootstrap/mc1.Rdata")
 
 p=100
 mc2<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
-save(mc2,file = "Estimation/Bootstrap/mc2.R")
+save(mc2,file = "Estimation/Bootstrap/mc2.Rdata")
 
 
 p=200
 mc3<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
-save(mc3,file = "Estimation/Bootstrap/mc3.R")
+save(mc3,file = "Estimation/Bootstrap/mc3.Rdata")
 
 
 p=50
 nonzero=10
 mc4<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid10")
-save(mc4,file = "Estimation/Bootstrap/mc4.R")
+save(mc4,file = "Estimation/Bootstrap/mc4.Rdata")
 
 p=100
 mc5<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid10")
-save(mc5,file = "Estimation/Bootstrap/mc5.R")
+save(mc5,file = "Estimation/Bootstrap/mc5.Rdata")
 
 
 p=200
 mc6<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid10")
-save(mc6,file = "Estimation/Bootstrap/mc6.R")
+save(mc6,file = "Estimation/Bootstrap/mc6.Rdata")
 
 
 
