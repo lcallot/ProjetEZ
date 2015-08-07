12fe13997f34b16924e1188f8e50200b2a7fedbb
clementcarrier <clement.carrier@ensae.fr>
2015-08-07 10:49:36 +0200

    maj

--- Estimation/Bootstrap/test2.R
+++ Estimation/Bootstrap/test2.R
@@ -10,8 +10,8 @@ source("Functions/lahiri.R")
 source("Functions/montecarlo.R")
 
 #2
-iter=10
-boot=10
+iter=1000
+boot=100
 n=100
 p=100
 nonzero=5
@@ -23,17 +23,4 @@ nonzero=5
 mc3<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
 save(mc3,file = "Estimation/Bootstrap/data/2/mc3.Rdata")
 
-## 3
-iter=1000
-boot=100
-n=100
-beta=5
-p=100
-nonzero=5
-mc2<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
-save(mc2,file = "Estimation/Bootstrap/data/3/mc2.Rdata")
-p=200
-nonzero=5
-mc3<-MC(iter,p,nonzero,beta,n,boot,0.1,"iid5")
-save(mc3,file = "Estimation/Bootstrap/data/3/mc3.Rdata")
 
