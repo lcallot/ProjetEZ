c8cf0f3adc03472414a3ec97272731feb80eb393
clementcarrier <clement.carrier@ensae.fr>
2015-08-12 14:22:45 +0200

    maj

--- Agrege/Replication/bigvar.R
+++ Agrege/Replication/bigvar.R
@@ -2,16 +2,16 @@ library(BigVAR)
 library(lattice)
 
 load("Agrege/Base/vardata.Rdata")
-#df<-var[,c("date", "YER", "PCR", "GCR", "ITR", "XTR", "MTR", "YED",  
-#           "GCD", "ITD", "XTD", "MTD", "YFD", "YIN", "WIN", "TIN", 
-#           "YFN",  "HICPSA", "URX", "STN", "LTN","POILU", 
-#           "PCOMU", "YWR","WRN",  "EEN", "EXR",  "M1",
-#           "M3", "ESI", "LIB", "PPI", "DJES")]
-
-df<-var[,c("date", "YER", "PCR", "GCR",   
-            "WIN", "HICPSA", "URX", "STN", "LTN","POILU", 
-           "PCOMU", "YWR",  "EEN", "EXR",  "M1",
+df<-var[,c("date", "YER", "PCR", "GCR", "ITR", "XTR", "MTR", "YED",  
+           "GCD", "ITD", "XTD", "MTD", "YFD",  "WIN",  
+           "YFN",  "HICPSA", "URX", "STN", "LTN","POILU", 
+           "PCOMU", "YWR","WRN",  "EEN", "EXR",  "M1",
            "M3", "ESI", "LIB", "PPI", "DJES")]
+
+#df<-var[,c("date", "YER", "PCR", "GCR",   
+#            "WIN", "HICPSA", "URX", "STN", "LTN","POILU", 
+#           "PCOMU", "YWR",  "EEN", "EXR",  "M1",
+#           "M3", "ESI", "LIB", "PPI", "DJES")]
 
 Ddf <- tail(df,-1) - head(df,-1)
 
@@ -41,18 +41,30 @@ df3<-as.matrix(df2,92,dim(df)[2])
 
 #load("/Users/carrierclement/Documents/Stage VU/BigVAR/BigVAR/data/Y.RData")
 
-bvdata<-constructModel(df3, p=4, "Lag",gran=c(50,10) , h = 1, cv = "Rolling")
-results=cv.BigVAR(bvdata)
+bvdata1<-constructModel(df3, p=4, "None",gran=c(50,10) , h = 1, cv = "Rolling")
+bvdata2<-constructModel(df3, p=4, "Lag",gran=c(50,10) , h = 1, cv = "Rolling")
+bvdata3<-constructModel(df3, p=4, "SparseLag",gran=c(50,10) , h = 1, cv = "Rolling")
+bvdata4<-constructModel(df3, p=4, "Diag",gran=c(50,10) , h = 1, cv = "Rolling")
+bvdata5<-constructModel(df3, p=4, "SparseDiag",gran=c(50,10) , h = 1, cv = "Rolling")
 
-save(results,file="Agrege/Replication/results2.Rdata")
+results1=cv.BigVAR(bvdata1)
+results2=cv.BigVAR(bvdata2)
+results3=cv.BigVAR(bvdata3)
+results4=cv.BigVAR(bvdata4)
+results5=cv.BigVAR(bvdata5)
 
+save(results1,file="Agrege/Replication/results1.Rdata")
+save(results2,file="Agrege/Replication/results2.Rdata")
+save(results3,file="Agrege/Replication/results3.Rdata")
+save(results4,file="Agrege/Replication/results4.Rdata")
+save(results5,file="Agrege/Replication/results5.Rdata")
 
-load("Agrege/Replication/results.Rdata")
-results
-str(results)
-plot(results)
-SparsityPlot.BigVAR.results(results)
+#load("Agrege/Replication/results2.Rdata")
+#results
+#str(results)
+#plot(results)
+#SparsityPlot.BigVAR.results(results)
 
-
+help(constructModel)
 
 
