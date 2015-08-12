f21a71148c0e06b075a7c14e89c6ba17e56cf8b2
clementcarrier <clement.carrier@ensae.fr>
2015-08-12 16:28:27 +0200

    maj

--- Agrege/Replication/postestimation.R
+++ Agrege/Replication/postestimation.R
@@ -30,10 +30,27 @@ for (i in 1:92){
   x[i]<-df$HICPSA[i+4]-df$HICPSA[i]
 }
 
-
-sub<- df[5:96,]
-
-sub$date<-NULL
-sub$HICPSA<-x
 #Dsub <- tail(sub,-1) - head(sub,-1)
+df$date<-NULL
+
+lag=4
+lv<-lassovar(df,lags=lag,adaptive="none", post=TRUE ,ncores=TRUE)
+
+dim(lv$post)
+
+cons<-lv$post[1,]
+coef<-matrix(lv$post[2:(lag*dim(df)[2]+1),],lag*dim(df)[2])
+
+h=12
+
+psy<-vector("list",h)
+psy[[1]]<-diag(x = 1, dim(df)[2], dim(df)[2])
+psy[[2]]<-psy[[1]]%*%coef[1:(dim(df)[2]),]
+psy[[3]]<-psy[[2]]%*%coef[1:(dim(df)[2]),] + psy[[1]]%*%coef[(dim(df)[2]+1):(2*dim(df)[2]),]
+
+for (i in 0:(h-1)){
+  
+
+   
+}
 
