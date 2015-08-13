d5e6eb2fc25d10b9a0a0e931d809c7d777cd886f
clementcarrier <clement.carrier@ensae.fr>
2015-08-13 11:11:36 +0200

    maj

--- Agrege/Replication/postestimation.R
+++ Agrege/Replication/postestimation.R
@@ -31,62 +31,17 @@ for (i in 1:92){
 }
 
 
-sub<- df[5:96,]
-
-sub$date<-NULL
-sub$HICPSA<-x
 #Dsub <- tail(sub,-1) - head(sub,-1)
 
 
 
-# bootlassovar
-iter=10
-adap="ridge"
-lag=12
-preforecast=28
-horizon=12
-TREND="no"
-
-
-
-bootcoef<-bootlassovar(sub[1:80,],lag,iter,adap,FALSE)
-Q1<-bootlassovar.prediction(sub[1:80,],bootcoef,lag,preforecast,horizon,"no")
-#save(Q1,file="Agrege/Replication/Q1.Rdata")
-
-name="HICPSA"
-plotfunction(Q1,name,preforecast+horizon,iter,"no",4)
-plot(x=seq(as.Date("2004/01/1"), as.Date("2013/12/1"), by = "quarter"),y=sub$HICPSA[53:92],type="l")
 
 
 
 
-plotfunction<-function(liste,name,lenght,iter,yoy,freq){
-  
-  e<-matrix(NA,lenght,iter)
-  for (i in 1:iter){
-    e[,i]<-liste[[i]][,name]
-  }
-  
-  if (yoy=="yes"){
-    f<-matrix(NA,lenght-freq,iter)
-    for (j in 1:(lenght-freq)){
-      f[j,]<-e[j+freq,]-e[j,]
-    }
-    
-    df<-data.frame(f)
-    time<-seq(as.Date("2005/1/1"), as.Date("2013/12/1"), by = "quarter")
-    df$time<-time
-    D=melt(df, id='time')
-    a<-ggplot(D,aes(time,value, group=variable, color=variable))+geom_line()
-    
-  } else {
-    
-    df<-data.frame(e)
-    time<-seq(as.Date("2004/1/1"), as.Date("2013/12/1"), by = "quarter")
-    df$time<-time
-    D=melt(df, id='time')
-    a<-ggplot(D,aes(time,value, group=variable, color=variable))+geom_line()
-  }
-  
-  return(a)
-}
+
+
+
+
+
+
